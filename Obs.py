57cd9508b94757045b56b6565f2ed1de196200e2
Douglas Schumacher <dschumacher4@wisc.edu>
2018-10-17 19:16:53 -0500

    Update Obs.py
    Got data function working.

--- Obs.py
+++ Obs.py
@@ -6,14 +6,13 @@ now = datetime.datetime.now()
 def main():
     menu_input = ""
     # Time format YYYYMMDDTTTT
-    starttime = "201809110000"
-    endtime = "201810111200"
     print("Great Lakes Weather Service Data Viewer, written by Douglas Schumacher")
     print("The current date and time is " + now.strftime("%Y-%m-%d %H:%M"))
-    starttime = "2018" + input("Enter the desired start time for your records: ") + "0000" # Midnight
-    endtime = "2018" + input("Enter the desired start time for your records: ") + "0000" # Midnight
+    starttime = "201810161200" # Noon yesterday (10/16)
+    endtime = currenttime() # Period ends at current time
     while menu_input != "q":
         menu_input = input("Enter 1 for Cranberry data, 2 for Wysocki data, and 3 for Buckley Data, or q to quit: ")
+        print("********")
         if menu_input == 'q':
             quit()
         if menu_input == '1':
@@ -25,10 +24,8 @@ def main():
 
 def cranberries(starttime, endtime):
     print("Not done yet.")
+    return
 def wysocki(starttime, endtime):
-    print("Precipitation reports")
-    # NOT WORKING YET
-    precip = m.precip(stid='AT644, KISW, NEHW3, UR380, C0617, KPCZ, NKAW3, E9270, KY50', start = starttime, end = endtime, units='precip|in')
     # Babcock, AT644 [Nekoosa]
     # Wisconsin Rapids, KISW
     # Nekoosa, AT644
@@ -39,41 +36,40 @@ def wysocki(starttime, endtime):
     # Rome, NKAW3
     # Hancock, E9270
     # Wautoma, KY50
-    for name in precip['STATION']:
-        print(name)
-    KUES_precip = precip['STATION'][2]
-    KMSN_precip = precip['STATION'][1]
-    KMKE_precip = precip['STATION'][0]
-    # Waukesha, KUES
-    station = KUES_precip['STID'] # remember we stored the dictionary in the precip variable
-    totalPrecip =  KUES_precip['OBSERVATIONS']['total_precip_value_1']
-    print('The total accumulated precip at ' + station + ' was ' + str(totalPrecip) + '"')
-    # Lakeshore, KMKE
-    station = KMKE_precip['STID'] # remember we stored the dictionary in the precip variable
-    totalPrecip =  KMKE_precip['OBSERVATIONS']['total_precip_value_1']
-    print('The total accumulated precip at ' + station + ' was ' + str(totalPrecip) + '"')
-    # Madison, KMSN
-    station = KMSN_precip['STID'] # remember we stored the dictionary in the precip variable
-    totalPrecip =  KMSN_precip['OBSERVATIONS']['total_precip_value_1']
-    print('The total accumulated precip at ' + station + ' was ' + str(totalPrecip) + '"')
+    station_list = ["AT644", "KISW", "NEHW3", "UR380", "C0617", "KPCZ", "NKAW3", "E9270", "KY50"]
+    for station in station_list:
+        data(station, starttime, endtime)
+
 def buckley(starttime, endtime):
-    # Getting data
-    station_list = ["KUES", "KMKE", "KMSN"]
+    station_list = ["KUES", "KMKE", "KMSN"] # Waukesha, Milwaukee, Madison
     for station in station_list:
         data(station, starttime, endtime)
 
 def data(station, starttime, endtime):
     precip = m.precip(stid = station, start = starttime, end = endtime, units='precip|in')
     climate = m.time_stats(stid= station, start = starttime, end = endtime, type = 'all')
-    station_climate = climate['STATION'][0]
-    station_precip = precip['STATION'][0]
-    # station_name = station_precip['STID'] # Storing station name to print
+    try:
+        station_climate = climate['STATION'][0]
+        station_precip = precip['STATION'][0]
+    except:
+        print("Sorry, data set incomplete for " + station)
+        print("-------")
+        return
+    station_name = station_climate['NAME'] # Storing station name to print
     totalPrecip = station_precip['OBSERVATIONS']['total_precip_value_1']
-    high_temp_c = station_climate['STATISTICS']['air_temp_set_1']['maximum']
-    low_temp_c = station_climate['STATISTICS']['air_temp_set_1']['minimum']
-    print('The total accumulated precip at ' + station + ' was ' + str(totalPrecip) + '"')
-    print("High: " + str(high_temp_c)) # High of period
-    print("Low: " + str(low_temp_c)) # Low of period
-    print("------")
+    high_temp = temp_f(station_climate['STATISTICS']['air_temp_set_1']['maximum'])
+    low_temp = temp_f(station_climate['STATISTICS']['air_temp_set_1']['minimum'])
+    print("Data for " + station_name + ", from " + str(starttime) + " to " + str(endtime))
+    print("High: " + str(high_temp) + " °F") # High of period
+    print("Low: " + str(low_temp) + " °F") # Low of period
+    print("Total precip: " + str(totalPrecip) + '"') # Total precip of period
+    print("-------")
+
+def temp_f(temp_c):
+    return ((9.0 / 5.0) * temp_c) + 32.0
+
+def currenttime():
+    return now.strftime("%Y%m%d%H%M")
+
 if __name__ == "__main__":
     main()
