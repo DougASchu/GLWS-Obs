72a8db0c9ff198f2663943a754917f7b721793f0
Douglas Schumacher <dschumacher4@wisc.edu>
2018-10-25 16:45:22 -0500

    Update Obs.py
    Added Cranberry temperature functionality

--- Obs.py
+++ Obs.py
@@ -8,7 +8,7 @@ def main():
     # Time format YYYYMMDDTTTT
     print("Great Lakes Weather Service Data Viewer, written by Douglas Schumacher")
     print("The current date and time is " + now.strftime("%Y-%m-%d %H:%M"))
-    starttime = "201810161200" # Noon yesterday (10/16)
+    starttime = "201810241800" # 6 PM yesterday (10/24)
     endtime = currenttime() # Period ends at current time
     while menu_input != "q":
         menu_input = input("Enter 1 for Cranberry data, 2 for Wysocki data, and 3 for Buckley Data, or q to quit: ")
@@ -23,7 +23,59 @@ def main():
             buckley(starttime, endtime)
 
 def cranberries(starttime, endtime):
-    print("Not done yet.")
+    # Sparta, AR731 [Cataract]
+    # Fort McCoy, KCMY
+    # Tomah, NO CURRENT STATION
+    # Camp Douglas, KVOK
+    # Black River Falls, KBCK
+    # Eau Claire, KEAU
+    # Augusta, AFWW3
+    # Neillsville, KMFI [Marshfield]
+    # Mather, NCHW3 [Necedah Wildlife Refuge]
+    region1_station_list = ["AR730", "KCMY", "TOMAH NO STATION", "KVOK", "KBCK", "KEAU", "AFWW3", "KMFI", "NCHW3" ]
+    # Town of Cranmoor, Elm Lake Cranberry, NOT IN MESOWEST
+    # Wisconsin Rapids, KISW
+    # Rome, NKAW3
+    # Mather, NCHW3 [Necedah Wildlife Refuge]
+    # Necedah, NEHW3
+    # Plover, PLOW3 [NOT WORKING], KSTE
+    # Lake Du Bay, Du Bay Cranberry, NOT IN MESOWEST
+    # Hancock, E9270
+    # Wautoma, KY50
+    region2_station_list = ["KISW", "NKAW3", "NCHW3", "NEHW3","KSTE", "E9270", "KY50"]
+    # Siren, KRZN
+    # Minong, MRZW3
+    # Haugen, HAUW3
+    # Barnes, BRNW3
+    # Cable, CABW3 [LOTS OF ERRORS RIGHT NOW]
+    # Clam Lake, CLRW3
+    # Hayward, KHYR
+    # Rice Lake, KRPD
+    # Bruce, DW1191
+    # Ladysmith, KRCX
+    region3_station_List = ["KRZN", "MRZW3", "HAUW3", "BRNW3", "CABW3", "CLRW3", "KHYR", "KRPD", "DW1191", "KRCX"]
+    # Hurley, KIWD
+    # Glidden, GDNW3
+    # Fifield, E4088
+    # Phillips, KPBH
+    # Manitowish Waters [Cranberry], NOT IN MESOWEST
+    # Manitowish Waters [Airport], KD25
+    # Minocqua, KARV
+    # Eagle River, KEGV
+    # Three Lakes, Sampson Cranberry, NOT IN MESOWEST
+    # Lake Nokomis [Cranberry], NOT IN MESOWEST
+    # Rhinelander, KRHI
+    # Tomahawk, KTKV
+    region4_station_list = ["KIWD", "GDNW3", "KPBH", "KD25", "KARV", "KEGV", "KRHI", "KTKV"]
+    master_station_list = [region1_station_list, region2_station_list, region3_station_List, region4_station_list]
+    i = 1
+    for station_list in master_station_list:
+        print("__________________")
+        print("Low Temperatures for Region " + str(i) + " from " + str(starttime) + " to " + str(endtime))
+        print("__________________")
+        i += 1
+        for station in station_list:
+            low_temp_data(station, starttime, endtime)
     return
 def wysocki(starttime, endtime):
     # Babcock, AT644 [Nekoosa]
@@ -63,6 +115,20 @@ def data(station, starttime, endtime):
     print("High: " + str(high_temp) + " °F") # High of period
     print("Low: " + str(low_temp) + " °F") # Low of period
     print("Total precip: " + str(totalPrecip) + '"') # Total precip of period
+    print("-------")
+
+def low_temp_data(station, starttime, endtime):
+    climate = m.time_stats(stid= station, start = starttime, end = endtime, type = 'all')
+    try:
+        station_climate = climate['STATION'][0]
+    except:
+        print("Sorry, data set incomplete for " + station)
+        print("-------")
+        return
+    station_name = station_climate['NAME'] # Storing station name to print
+    low_temp = temp_f(station_climate['STATISTICS']['air_temp_set_1']['minimum'])
+    print("Data for " + station_name)
+    print("Low: " + str(low_temp) + " °F") # Low of period
     print("-------")
 
 def temp_f(temp_c):
